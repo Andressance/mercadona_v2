5627115c67c573206e6227d80ccb874ad14555ee
Sawe <149082520+Saul1727@users.noreply.github.com>
2025-11-10 15:18:49 +0100

    hola

--- src/services/list.js
+++ src/services/list.js
@@ -1,6 +1,7 @@
 import { collection, addDoc, doc, getDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "./firebase.js";
 import jsPDF from 'jspdf';
+import jsPDF from 'jspdf';
 
 export const getUserUidFromFirestore = async (authUid) => {
   const userRef = doc(db, "users", authUid); // asumimos que el docId = authUid
@@ -22,16 +23,16 @@ export const createNewList = async (ownerUid, products) => {
   });
   return docRef.id;
 };
-
 export const generatePdf = (items) => {
   const doc = new jsPDF();
   doc.setFontSize(16);
   doc.text("Mi lista de compra", 10, 10);
 
   items.forEach((item, index) => {
+    // --- CORRECCIÓN ---
+    // Se han cambiado las comillas simples ' por backticks `
     doc.text(`${index + 1}. ${item.name} x${item.quantity}`, 10, 20 + index * 10);
   });
 
   doc.save("mi_lista.pdf");
 };
-
